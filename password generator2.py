c842c5982542b37e344b96497845514d527c7f78
KARUNYAAC <168293868+KARUNYAAC@users.noreply.github.com>
2024-04-28 11:53:42 +0530

    Add files via upload

---
+++ password generator2.py
@@ -0,0 +1,39 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\ELCOT\AppData\Local\Programs\Python\Python39\password generator.py
+Enter password length: 10
+Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit
+Pick a number 1
+Pick a number 3
+Pick a number 2
+Pick a number 4
+The random password is )WMry%J?~t
+>>> 
+========================================= RESTART: C:\Users\ELCOT\AppData\Local\Programs\Python\Python39\password generator.py ========================================
+Enter password length: 8
+Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit
+Pick a number 1
+Pick a number 2
+Pick a number 4
+The random password is SinjodaB
+>>> 
+========================================= RESTART: C:\Users\ELCOT\AppData\Local\Programs\Python\Python39\password generator.py ========================================
+Enter password length: 9
+Choose character set for password from these : 
+		1. Digits
+		2. Letters
+		3. Special characters
+		4. Exit
+Pick a number 1
+Pick a number 4
+The random password is XmHkpsUHF
+>>> 
